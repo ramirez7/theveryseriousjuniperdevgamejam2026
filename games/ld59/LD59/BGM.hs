9ef0a8e43cdd4f92ec518000b0d2906bd30d5af2
Armando Ramirez <armando.m.ramirez@gmail.com>
2026-05-07 11:46:21 -0700

    bgm module

---
+++ games/ld59/LD59/BGM.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE RecordWildCards #-}
+module LD59.BGM where
+
+import LD59.Env
+import LD59.World
+import LD59.Jfxr.JSFFI qualified as Jfxr
+import Apecs
+import Data.Foldable (traverse_)
+import LD59.Art
+
+switchBGM :: HasEnv => Screen -> System World ()
+switchBGM screen = openEnv $ \Env{..} -> do
+  Apecs.get global >>= (liftIO . traverse_ Jfxr.stopAudioBSN . bgmAudio)
+
+  let toPlay = case screen of
+        Title -> Nothing
+        Playing -> Just (artPlayingBGM envArt)
+        Dead -> Just (artGameoverBGM envArt)
+
+  bgm <- liftIO $ traverse (Jfxr.loopAudioBuffer envAudio) toPlay
+
+  Apecs.set global $ BGM bgm
+
