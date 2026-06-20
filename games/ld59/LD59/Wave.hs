f84b7d5dd8b0464f78ad30329d1a7078a854a87c
Armando Ramirez <armando.m.ramirez@gmail.com>
2026-06-20 16:16:53 -0700

    gut wave.hs

--- games/ld59/LD59/Wave.hs
+++ games/ld59/LD59/Wave.hs
@@ -1,19 +1,4 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
 module LD59.Wave where
-
-import Lib
-import Pixi.Types qualified as Pixi
 
 data Wave = TRI | SIN | SQUARE | SAW | TAN
   deriving (Show, Eq, Ord, Enum, Bounded)
-
-waveSpriteTint :: Wave -> Pixi.Sprite -> IO ()
-waveSpriteTint w s = setProperty "tint" s (stringAsVal tint)
-  where
-    tint = case w of
-      TRI -> "yellow"
-      SIN -> "blue"
-      SQUARE -> "green"
-      SAW -> "red"
-      TAN -> "orange"
